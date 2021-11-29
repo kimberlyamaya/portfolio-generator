261b1486d7a5fecbf015e4f1dc5f25dc0f3ecd6d
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-11-28 22:12:53 -0800

    inquirer

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
+const inquirer = require('inquirer');
+
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then (answers => console.log(answers));
+
 // use this at top of file in order to use file system functions/modules
-const fs = require('fs');
+//const fs = require('fs');
 
 // added the generatePage function to the page-template.js and now we're making the connection
 // using this require
-const generatePage = require('./src/page-template.js');
+//const generatePage = require('./src/page-template.js');
 
 // holds user command-line arguments
-const profileDataArgs = process.argv.slice(2);
+//const pageHTML = generatePage(name, github)
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+/*fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!')
-});
+})*/
 
 /*// notice the lack of parentheses around the 'profileDataArr' parameter?
 const printProfileData = profileDataArr => {
